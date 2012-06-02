43b8958ff034ba11f6ba16915f5ef84bb54c5a6f
liors <lior@billguard.com>
2012-06-02 21:54:27 +0300

    new posts

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 module Nesta
   class App
     
-    get '/lior' do
-      'foo'
+    get '/effectiveIT' do
+      erb :effectiveIT
     end
 
-
+    
+    get '*' do
+      set_common_variables
+      parts = params[:splat].map { |p| p.sub(/\/$/, '') }
+      @page = Nesta::Page.find_by_path(File.join(parts))
+      raise Sinatra::NotFound if @page.nil?
+      @title = @page.title
+      @articles = Page.find_articles.select { |a| a.date }[0..9]
+      set_from_page(:description, :keywords)
+      cache haml(@page.template, :layout => @page.layout)
+    end
 
   end
 end
